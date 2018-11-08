6e97f5b62b45f497dcaad821a10e20bb0779b366
rzol <os@iosr.local>
2018-11-08 13:57:48 +0800

    Update

---
+++ RZBaseProject/RZBaseProject/Code/Lib/Category(类别)/UIView/UIView+RZFrame.h
@@ -0,0 +1,13 @@
+//
+//  UIView+RZFrame.h
+//  RZBaseProject
+//
+//  Created by os on 2018/11/1.
+//  Copyright © 2018 RZOL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (RZFrame)
+
+@end
---
+++ RZBaseProject/RZBaseProject/Code/Lib/Category(类别)/UIView/UIView+RZFrame.m
@@ -0,0 +1,13 @@
+//
+//  UIView+RZFrame.m
+//  RZBaseProject
+//
+//  Created by os on 2018/11/1.
+//  Copyright © 2018 RZOL. All rights reserved.
+//
+
+#import "UIView+RZFrame.h"
+
+@implementation UIView (RZFrame)
+
+@end
